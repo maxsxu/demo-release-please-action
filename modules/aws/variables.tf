c608456a460d9bae6dfe3ee7d3a7dc41d09f368d
Max Xu <xuhuan@live.cn>
2024-07-05 11:15:58 +0800

    fix: use block

--- modules/aws/variables.tf
+++ modules/aws/variables.tf
@@ -1,6 +1,8 @@
 variable "sn_policy_version" {
   description = "The value of SNVersion tag"
-  default     = "0.4.0"   # x-release-please-version
+// x-release-please-start-version
+  default     = "0.4.0"
+// x-release-please-end
   type        = string
 }
 
