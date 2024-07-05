f60e9b0debc091fe76aac2c133bcee3a84575b3f
Max Xu <xuhuan@live.cn>
2024-07-05 08:23:32 +0800

    Update

---
+++ modules/aws/variables.tf
@@ -0,0 +1,9 @@
+variable "sn_policy_version" {
+  description = "The value of SNVersion tag"
+  default     = "0.4.0"   // {x-release-please-version}
+  type        = string
+}
+
+variable "arg1" {
+
+}
